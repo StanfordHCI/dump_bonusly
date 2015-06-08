1bd4ac1fcfb4979ddc6046509fb367f73c64ca85
Geza Kovacs <gkovacs@stanford.edu>
2015-06-07 20:38:00 -0700

    fixed to use directed graph

--- compute_pagerank.py
+++ compute_pagerank.py
@@ -2,18 +2,36 @@
 
 import networkx as nx
 import json
+from collections import Counter
 
-G = nx.Graph()
+G = nx.DiGraph()
 
 badges = json.load(open('bonusly_badges.json'))['data']['result']
 
-edges = [(x['giver']['username'], x['receiver']['username']) for x in badges]
+edges = [(x['giver']['short_name'], x['receiver']['short_name']) for x in badges]
 
-G.add_edges_from(edges)
+edge_weights = Counter()
+for giver,receiver in edges:
+  edge_weights[giver + '_' + receiver] += 1
 
-pagerank_results = nx.algorithms.link_analysis.pagerank_alg.pagerank(G)
+weighted_edges = []
+added_edges = set()
+for giver,receiver in edges:
+  edgename = giver + '_' + receiver
+  if edgename in added_edges:
+    continue
+  added_edges.add(edgename)
+  weighted_edges.append((giver, receiver, edge_weights[edgename]))
+
+user_to_num_badges = Counter()
+for giver,receiver in edges:
+  user_to_num_badges[receiver] += 1
+
+G.add_weighted_edges_from(weighted_edges)
+
+pagerank_results = nx.algorithms.link_analysis.pagerank_alg.pagerank(G, alpha=0.9, max_iter=10000)
 
 score_and_username = [(score,username) for username,score in pagerank_results.items()]
 
 for score,username in sorted(score_and_username, reverse=True):
-  print username, score
+  print username, 'score=' + str(score), 'badges=' + str(user_to_num_badges[username])
